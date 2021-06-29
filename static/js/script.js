5eee386a1c617ba62bcbefff39507ff74b198654
sanju <sk079997@gmail.com>
2021-06-29 23:28:33 +0530

    all done

--- static/js/script.js
+++ static/js/script.js
@@ -4,37 +4,6 @@ function thankyou(){
     
 }
 
-
-// var slideIndex = 0;
-// carousel();
-
-// function carousel(){
-//     var i;
-//     var x = document.getElementsByClassName("skill");
-//     if(window.innerWidth<700){console.log(window.innerWidth);
-//         for(i=0;i<x.length;i++){
-//             x[i].style.display = 'none';
-//         }
-//         slideIndex++;
-//         if(slideIndex> x.length)
-//         {slideIndex= 1}
-//         x[slideIndex-1].style.display='flex';
-        
-//     }
-//     else{
-//         for(i=0;i<x.length;i++){
-//             x[i].style.display = 'flex';
-//         }
-//     }
-// }
-// setTimeout(carousel,2000);
-// window.addEventListener('resize',()=>{
-//     if(window.innerWidth<700){
-//         carousel();
-//     }
-// });
-// window.addEventListener('onload',carousel);
-
 var counter=1;
 setInterval(
     function(){
@@ -43,4 +12,4 @@ setInterval(
         if(counter>3){
             counter=1;
         }
-    },2000);
+    },4000);
